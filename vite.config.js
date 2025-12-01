2442bfd8d90f010fea1bb1dfcdf5819b15fdb857
Jun-77 <ghkdqhdmswns@naver.com>
2025-12-01 21:34:15 +0900

    fix: vercel serverless function 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',       // Vercel이 읽는 빌드 output dir
+    emptyOutDir: true,    // 항상 깨끗하게
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5000', // dev only
         changeOrigin: true,
       },
     },
